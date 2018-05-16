8c8c4b92ad8b51635f2badb671f717d4151061b6
Tomás Ahumada <tomyahu@gmail.com>
2018-05-16 14:15:00 -0400

    second commit

---
+++ generador_archivos/twurl.py
@@ -0,0 +1,31 @@
+import urllib
+import oauth
+import hidden
+
+def augment(url, parameters) :
+    secrets = hidden.oauth()
+    consumer = oauth.OAuthConsumer(secrets['consumer_key'], secrets['consumer_secret'])
+    token = oauth.OAuthToken(secrets['token_key'],secrets['token_secret'])
+
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, 
+        token=token, http_method='GET', http_url=url, parameters=parameters)
+    oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, token)
+    return oauth_request.to_url()
+
+
+def get_last_n_tweets_from(n, user) :
+    print '* Calling Twitter...'
+    url = augment('https://api.twitter.com/1.1/statuses/user_timeline.json',
+        {'screen_name': user, 'count': str(n)} )
+    print url + '\n'
+    connection = urllib.urlopen(url)
+    data = connection.read()
+	
+    print data + '\n'
+    headers = connection.info().dict
+    print headers
+    return data
+	
+file = open("200_tweets.json", "w")
+file.write(get_last_n_tweets_from(200, 'TomimiDraws'))
+file.close()
