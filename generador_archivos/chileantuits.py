9f20d2e9933800b490bcee83348c1140bf1a757e
tomyahu <tomyahu@gmail.com>
2018-06-14 18:35:19 -0400

    edited so it can strore tweets in a file

--- generador_archivos/chileantuits.py
+++ generador_archivos/chileantuits.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import tweepy
 import hidden
+import time
 
 secrets = hidden.oauth()
 consumer_key = secrets['consumer_key']
@@ -10,9 +14,20 @@ token_secret = secrets['token_secret']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(token_key, token_secret)
 
+api = tweepy.API(auth)
+
 # Place id obtenido por geo api. Extraído de /data/locations.json
 place_id = "47a3cf27863714de"
-tweets = api.search(q="place:%s" % place_id)
-for tweet in tweets:
-	print(tweet.text + " | " + tweet.place.name if tweet.place else "Undefined place")
 
+file = open("../data/tweets_recolectados_x_benja.txt","w")
+
+for i in range(4):
+	tweets = api.search(q="place:%s" % place_id)
+	for tweet in tweets:
+		file.write(emoji.demojize(tweet.text) + " | " + tweet.place.name if tweet.place else "Undefined place")
+		file.write("\n")
+	
+	time.sleep(10)
+	print(str((i+1)*5) + " segundos")
+	
+file.close()
