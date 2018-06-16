81ea80f1ba207757cffc7e1f9f67afa88ca92203
Benjamín Hurtado Torrealba <bhurtado@localhost.localdomain>
2018-06-15 22:36:33 -0400

    Gets tweets from Chile in live

---
+++ generador_archivos/chileantuits.py
@@ -0,0 +1,43 @@
+import tweepy
+import hidden
+import csv
+
+secrets = hidden.oauth()
+consumer_key = secrets['consumer_key']
+consumer_secret = secrets['consumer_secret']
+token_key = secrets['token_key']
+token_secret = secrets['token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(token_key, token_secret)
+
+api = tweepy.API(auth)
+
+tuitsFile = open('chileantuits.tsv', 'a')
+csvWriter = csv.writer(tuitsFile, delimiter='\t')
+
+
+def writeTuit(tuit):
+    tuitId = tuit.id
+    tuitAuthor = tuit.user.screen_name
+    tuitTimestamp = tuit.created_at
+    tuitText = tuit.text
+    tuitPlace = tuit.place.name
+    csvWriter.writerow([tuitId, tuitAuthor, tuitTimestamp, tuitText, tuitPlace])
+    print("[{}, {}] <{}>: {}".format(tuitTimestamp, tuitPlace, tuitAuthor, tuitText))
+    
+
+#Override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        if(status.place.country == "Chile"):
+            writeTuit(status)
+
+    def on_error(self, status_code):
+        tuitsFile.close()
+        print(str(status_code))
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream.filter(locations=[-109.4791708,-56.5573577,-66.15203,-17.497384])
